2369edeb6677ad00005f03f335cedd38b2e8c5db
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-06-13 18:42:56 -0400

    AL FIN

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,34 @@
-    (() => {
-      const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector(".modal__close"),
-        modal: document.querySelector("[data-modal]"),
-      };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector(".modal__close"),
+    modal: document.querySelector("[data-modal]"),
+    menuIcon: document.querySelector(".Hero__menu-icon"),
+    menu: document.querySelector(".Hero__menu")
+  };
 
-      refs.openModalBtn.addEventListener("click", toggleModal);
-      refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.menuIcon.addEventListener("click", toggleMenu);
 
-      function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-      }
-    })();
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".Hero__menu-icon"),
+    menu: document.querySelector(".Hero__menu"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
